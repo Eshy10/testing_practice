1d01b9c35b7ba16c78e128856cda3eb3bb1e39b6
SpaYco <zizou.aziz101@gmail.com>
2020-08-10 14:59:54 +0100

    fixed funciton not returning

--- src/reverse.js
+++ src/reverse.js
@@ -1,6 +1,7 @@
 const reverseString = (string) => {
     let newStr = ''
-    for (let i = (string.length - 1); i < 0; i -= 1) {
+    for (let i = (string.length - 1); i >= 0; i -= 1) {
         newStr += string[i];
     }
+    return newStr
 }
